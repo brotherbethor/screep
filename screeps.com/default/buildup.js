7f06eee5307405fa3e7c15316bba32630200e462
Boris Schulz <bschulz@Lucuma.local>
2018-01-01 21:39:30 +0100

    roads should be built on their own now

--- screeps.com/default/buildup.js
+++ screeps.com/default/buildup.js
@@ -1,47 +1,82 @@
+function road_data(){
+    var spawn = Game.spawns.Spawn1;
+    var controller = Game.spawns.Spawn1.room.controller;
+    var froms = [spawn, controller];
+    var tos = Game.spawns.Spawn1.room.find(FIND_SOURCES_ACTIVE);
+    var roads = {};
+    froms.forEach(
+        function(origin){
+            tos.forEach(
+                function(destination){
+                    var name = origin.id + "_to_" + destination.id;
+                    console.log('road name' + name);
+                    roads[name] = {
+                        'from': origin,
+                        'to': destination
+                    };
+                }
+            );
+        }
+    );
+    console.log('road length ' + roads.length);
+    return roads;
+}
+
 module.exports = {   
     roads: function() {
-        // TODO: erst nach mindestens 5 extensions
-        var roadName = "spawn1ToSource1";
-        if (typeof Memory.roads == "undefined"){
-            Memory.roads.spawn1ToSource1 = false;
-        }
-        if (Memory.roads.spawn1ToSource1 == true) {
-            return;
-        }
-        // var xpos = Game.spawns.Spawn1.pos;
-        // xpos.y = xpos.y - 1;
-        var waypoints = Game.spawns.Spawn1.room.findPath(
-            Game.spawns.Spawn1.pos,
-            Game.spawns.Spawn1.room.find(FIND_SOURCES_ACTIVE)[1].pos,
-            {"ignoreCreeps": true}
-        );
-        waypoints.forEach(
-            function(waypoint){
-                Game.spawns.Spawn1.room.createConstructionSite(
-                    waypoint.x,
-                    waypoint.y,
-                    STRUCTURE_ROAD
-                );
+        if (typeof Memory.roads_built == 'undefined') {
+            Memory.roads_built = 0;
+            Memory.road_building_max = 5;
+            Memory.road_building_probability = 0.999; // rebuild every 1000 ticks or so
+            console.log('Initialized road building data.');
+        } else {
+
+            if (Memory.current_state['extensions'] >= 5) {
+                if ((Memory.roads_built == 0) ||
+                    (Math.random() >= Memory.road_building_probability)) {
+                    var roads = road_data();
+                    for (var road_name in roads) {
+                        var coordinates = roads[road_name];
+                        var origin = coordinates['from'];
+                        var destination = coordinates['to'];
+                        var waypoints = Game.spawns.Spawn1.room.findPath(
+                            origin.pos,
+                            destination.pos,
+                            {'ignoreCreeps': true} // only ever build the default roads
+                        );
+                        waypoints.forEach(
+                            function(waypoint){
+                                Game.spawns.Spawn1.room.createConstructionSite(
+                                    waypoint.x,
+                                    waypoint.y,
+                                    STRUCTURE_ROAD
+                                );
+                            }
+                        );
+                        console.log('road built');
+                    }
+                    Memory.roads_built += 1;
+                }
             }
-        );
-        Memory.roads.spawn1ToSource1 = true;
+
+        }
     },
     extensions: function() {
         if (
-            (Memory.current_state["extensions"] <5) &&
-            (Memory.current_state["rcl"] == 2) && 
-            (Memory.current_state["construction_sites_extensions"] == 0)
+            (Memory.current_state['extensions'] < 5) &&
+            (Memory.current_state['rcl'] == 2) && 
+            (Memory.current_state['construction_sites_extensions'] == 0)
             ) {	
         	var sx = Game.spawns.Spawn1.pos.x;
     	    var sy = Game.spawns.Spawn1.pos.y;
-    	    var ex = sx - 2 + Memory.current_state["extensions"];
+    	    var ex = sx - 2 + Memory.current_state['extensions'];
     	    var ey = sy + 2;
     	    
             Game.spawns.Spawn1.room.createConstructionSite(
     	      	ex,ey,STRUCTURE_EXTENSION
     	    );
-            Memory.current_state["extensions"] += 1;
-    	    console.log("Built extension at " + ex + ":" + ey);
+            Memory.current_state['extensions'] += 1;
+    	    console.log('Built extension at ' + ex + ':' + ey);
         }
     }
 };
