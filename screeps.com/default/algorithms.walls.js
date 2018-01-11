4dfae0c6adfc0a23841a4ec72d28d1b7c051af40
Boris Schulz <bschulz@Lucuma.local>
2018-01-11 13:16:12 +0100

    i can mark all exits :-)

--- screeps.com/default/algorithms.walls.js
+++ screeps.com/default/algorithms.walls.js
@@ -1,11 +1,19 @@
 const room_size = 50;
+const X = 0;
+const Y = 1;
 
 function remove_flags() {
     Game.spawns.Spawn1.room.createFlag(x, y);
 }
 
 function set_flag(x, y) {
-    Game.spawns.Spawn1.room.createFlag(x, y);
+    var things = Game.spawns.Spawn1.room.lookAt(x, y);
+    if (things.length != 1) {return false;}
+    if ((things[0]['type'] == 'terrain') && (things[0]['terrain'] == 'wall')) {
+        return false;
+    } else {
+        Game.spawns.Spawn1.room.createFlag(x, y);
+    }
 }
 
 function markExits(){
@@ -23,6 +31,24 @@ function markExits(){
     }
     Memory.exits_marked = true;
 }
+
+
+function countExits(){
+    if (typeof Memory.exits_marked != 'undefined') {
+        return false;
+    }
+    for (x = 0; x < room_size; x++) {
+        for (y = 0; y < room_size; y++){
+            if ((x == 0) || (x == (room_size -1))) {
+                set_flag(x,y);
+            } else if ((y == 0) || (y == (room_size -1))) {
+                set_flag(x,y);
+            }
+        }
+    }
+    Memory.exits_marked = true;
+}
+
 
 exports.markExits = markExits;
 exports.remove_flags = remove_flags;
